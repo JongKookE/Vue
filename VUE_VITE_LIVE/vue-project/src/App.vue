fba70e4a6a7f6195ea811bfa88aeb40cb4a5c28f
JongKook <whdrnrdl789@naver.com>
2023-11-09 21:32:43 +0900

    Router

--- VUE_VITE_LIVE/vue-project/src/App.vue
+++ VUE_VITE_LIVE/vue-project/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import {RouterView} from "vue-router"
 import NavbarPage from "./components/NavbarPage.vue";
 import FooterPage from "./components/FooterPage.vue";
+import AboutPage from "./components/AboutPage.vue"
 </script>
 
 <template>
     <navbar-page></navbar-page>
+    <router-view></router-view>
     <footer-page></footer-page>
 </template>
 
